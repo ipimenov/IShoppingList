78e7848b7ce0975860d98f5bce195275c61838b8
Maverick <bravo1@list.ru>
2023-05-18 14:18:00 +0300

    Initial commit

---
+++ app/src/main/java/ru/ipimenov/training/ishoppinglist/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.ipimenov.training.ishoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
